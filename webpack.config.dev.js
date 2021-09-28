192b546dc12278d6f7ea733ceb150972b5793472
Alek Ortiz <65991139+aleksebastian@users.noreply.github.com>
2021-09-27 20:22:22 -0500

    Initial commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,34 @@
+module.exports = {
+  mode: 'development',
+  entry: __dirname + '/client/src/index.js',
+  module: {
+    rules: [
+      {
+        test: [/\.jsx$|.js$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/dist'
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: __dirname + '/client/dist',
+    compress: true,
+    open: true,
+    // To open with your browser of choice comment line above and uncomment lines below
+    // open: {
+    //   app: {
+    //     name: 'Brave Browser',
+    //   },
+    // },
+  },
+};
