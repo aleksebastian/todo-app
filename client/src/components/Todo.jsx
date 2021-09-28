38c2a4844dab30b52d99619f408175060e8a0e6c
Alek <aleksebastiano@gmail.com>
2021-09-27 21:51:24 -0500

    Created simple to do list from template

---
+++ client/src/components/Todo.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import TodoForm from "./TodoForm.jsx";
+import { RiCloseCircleLine } from "react-icons/ri";
+import { TiEdit } from "react-icons/ti";
+
+function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: "",
+  });
+
+  const submitUpdate = (val) => {
+    updateTodo(edit.id, val);
+    setEdit({
+      id: null,
+      value: "",
+    });
+  };
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
+  return todos.map((todo, i) => (
+    <div className={todo.isComplete ? "todo-row complete" : "todo-row"} key={i}>
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className="icons">
+        <RiCloseCircleLine
+          className="delete-icon"
+          onClick={() => removeTodo(todo.id)}
+        />
+        <TiEdit
+          className="update-icon"
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+        />
+      </div>
+    </div>
+  ));
+}
+
+export default Todo;
